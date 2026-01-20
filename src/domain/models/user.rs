2d18f9661b7a453b34ff638ec647a50cb54962aa
Vitalij-Burk <burkasovvitalij@gmail.com>
2026-01-20 07:00:20 +0600

    New models logic template

--- src/domain/models/user.rs
+++ src/domain/models/user.rs
@@ -27,3 +27,57 @@ impl User {
         }
     }
 }
+
+#[derive(Debug, Clone, Deserialize, Serialize, sqlx::FromRow)]
+pub struct UserEntity {
+    pub id: i32,
+
+    pub key: String,
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct UserD {
+    pub id: i32,
+
+    pub key: String,
+    pub name: String,
+}
+
+impl UserD {
+    pub fn new(key: &str, name: &str) -> Self {
+        let id = rand::rng().random::<i32>();
+
+        Self {
+            id: id,
+            key: key.to_string(),
+            name: name.to_string().to_title_case(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct UserDTO {
+    pub id: i32,
+
+    pub key: String,
+    pub name: String
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct CreateUserDTO {
+    pub key: String,
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct UpdateUserDTO {
+    pub key: Option<String>,
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum DeleteUserDTO {
+    ById { id: i32 },
+    ByKey { key: String },
+}
--- src/domain/models/word_pair.rs
+++ src/domain/models/word_pair.rs
@@ -41,3 +41,69 @@ impl WordPair {
         }
     }
 }
+
+#[derive(Debug, Clone, Deserialize, Serialize, sqlx::FromRow)]
+pub struct WordPairEntity {
+    pub id: i32,
+    pub user_id: i32,
+
+    pub target_text: String,
+    pub source_text: String,
+    pub target_language: String,
+    pub source_language: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct WordPairDomain {
+    pub id: i32,
+    pub user_id: i32,
+
+    pub target_text: String,
+    pub source_text: String,
+    pub target_language: String,
+    pub source_language: String,
+}
+
+impl WordPairDomain {
+    pub fn new(
+        user_id: &i32,
+        target_text: &str,
+        source_text: &str,
+        target_language: &str,
+        source_language: &str,
+    ) -> Self {
+        let id = rand::rng().random::<i32>();
+
+        Self {
+            id: id,
+            user_id: *user_id,
+            target_text: target_text.to_string().to_title_case(),
+            source_text: source_text.to_string().to_title_case(),
+            target_language: target_language.to_string().to_lowercase(),
+            source_language: source_language.to_string().to_lowercase(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct WordPairDTO {
+    pub id: i32,
+
+    pub target_text: String,
+    pub source_text: String,
+    pub target_language: String,
+    pub source_language: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct CreateWordPairDTO {
+    pub target_text: String,
+    pub source_text: String,
+    pub target_language: String,
+    pub source_language: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum DeleteWordPairDTO {
+    ById { id: i32 },
+}
